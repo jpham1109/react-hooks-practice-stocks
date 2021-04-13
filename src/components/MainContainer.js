38c899dfdc17fddbc95d036aec18ba2e952b1449
Jamie H Pham <jpham1109@gmail.com>
2021-04-13 18:07:14 -0400

    Done plus bonus

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks ] = useState([])
+  const [sort, setSort] = useState("")
+  const [filter, setFilter] = useState("All")
+  const [portfolio, setPortfolio] = useState([])
+
+ 
+  useEffect (() => {
+    fetch("http://localhost:3001/stocks")
+    .then(r => r.json())
+    .then(setStocks)
+  }, [])
+
+  function handleSort(event) {
+    setSort(event.target.value)
+  }
+
+  function handleFilter(event) {
+    setFilter(event.target.value)
+  }
+
+  function handleAddStock(stockToAdd) {
+    const stockInPortfolio = portfolio.find((stock) => stock.id === stockToAdd.id )
+    if (!stockInPortfolio) {
+      setPortfolio([...portfolio, stockToAdd])
+    }
+  }
+
+  function handleRemoveStock(stockToRemove) {
+    setPortfolio(portfolio => portfolio.filter((stock) => stock.id !== stockToRemove.id))
+  }
+
+  const stocksToDisplay = [...stocks].sort((stockA, stockB) => {
+    if (sort === "Alphabetically") {
+      return stockA.ticker.localeCompare(stockB.ticker)
+    } else if (sort === "Price") {
+      return stockA.price - stockB.price
+    } else {
+      return stockA, stockB
+    }
+  })
+  .filter((stock) => {
+    if (filter !== "All") {
+      return stock.type === filter
+    } else {
+      return stock
+    }
+  })
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar onSort={sort} onSortCheck={handleSort} onFilter={filter} onFilterSelect={handleFilter} />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocksToDisplay} onStockClick={handleAddStock}/>
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer portfolio={portfolio} onRemoveStock={handleRemoveStock}/>
         </div>
       </div>
     </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-function SearchBar() {
+function SearchBar({onSort, onSortCheck, onFilter, onFilterSelect}) {
   return (
-    <div>
+    <div >
       <strong>Sort by:</strong>
       <label>
         <input
           type="radio"
           value="Alphabetically"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={onSort === "Alphabetically"}
+          onChange={onSortCheck}
         />
         Alphabetically
       </label>
@@ -19,15 +19,16 @@ function SearchBar() {
           type="radio"
           value="Price"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={onSort === "Price"}
+          onChange={onSortCheck}
         />
         Price
       </label>
       <br />
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select value={onFilter} onChange={onFilterSelect}>
+          <option value="All">All</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
